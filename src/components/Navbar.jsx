83167ae024a5cf39cf36f5c7c6f381241a1a7aa8
Mateusz Kopij <sagasu@gmail.com>
2021-09-26 13:11:36 +0100

    adding react-router-dom dependency and navbar component.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Button, Menu, Typography, Avatar} from 'antd';
+import {Link} from 'react-router-dom';
+import {MenuOutlined, FundOutlined, BulbOutlined, MoneyCollectOutlined, HomeOutlined} from '@ant-design/icons';
+import icon from '../images/crypto.png';
+
+function Navbar() {
+    return (
+        <div className="nav-container">
+            <div className="logo-container"></div>
+            <Avatar src={icon} size="large" />
+            <Typography.Title level={2} className="logo">
+                <Link to="/">Crypto currencies</Link>
+            </Typography.Title>
+            <Button className="menu-control-container">
+
+            </Button>
+        </div>
+    )
+}
+
+export default Navbar
