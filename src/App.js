9730b7713c99822ae5ed04ca6dc610810dc924a9
Mateusz Kopij <sagasu@gmail.com>
2021-09-26 19:06:00 +0100

    Adding basic components and routes to it.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Switch, Route, Link} from 'react-router-dom';
 import {Layout, Typography, Space} from 'antd';
-import {Navbar} from './components';
+import {Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails} from './components';
 import './App.css';
 
 const app = () => {
@@ -10,7 +10,19 @@ const app = () => {
             <div className="navbar">
                 <Navbar></Navbar>
             </div>
-            <div className="main"></div>
+            <div className="main">
+                <Layout>
+                    <div className="routes">
+                        <Switch>
+                            <Route exact path="/"><Homepage /></Route>
+                            <Route exact path="/exchanges"><Exchanges /></Route>
+                            <Route exact path="/cryptocurrencies"><Cryptocurrencies /></Route>
+                            <Route exact path="/crypto/:coinId"><CryptoDetails /></Route>
+                            <Route exact path="/news"><News /></Route>
+                        </Switch>
+                    </div>
+                </Layout>
+            </div>
             <div className="footer"></div>
             
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,9 +13,18 @@ function Navbar() {
                 <Link to="/">Crypto currencies</Link>
             </Typography.Title>
             <Menu theme="dark">
-                <Menu.Title icon={<HomeOutlined />}>
-                    <Link></Link>
-                </Menu.Title>
+                <Menu.Item icon={<HomeOutlined />}>
+                    <Link to="/">Home</Link>
+                </Menu.Item>
+                <Menu.Item icon={<FundOutlined />}>
+                    <Link to="/cryptocurrencies">Crypto currencies</Link>
+                </Menu.Item>
+                <Menu.Item icon={<MoneyCollectOutlined />}>
+                    <Link to="/exchanges">Exchanges</Link>
+                </Menu.Item>
+                <Menu.Item icon={<BulbOutlined />}>
+                    <Link to="/news">News</Link>
+                </Menu.Item>
             </Menu>
         </div>
     )
