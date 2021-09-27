8517fe5d588244f793d7a0e395b55947e6031fc5
Mateusz Kopij <sagasu@gmail.com>
2021-09-27 15:48:34 +0100

    adding news section and integration with bing

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -22,9 +22,10 @@ useEffect(() => {
 
     return (
         <>
-            <div className="search-crypto">
+            {!simplified && (
+                <div className="search-crypto">
                 <Input placeholder="Search Cryptocurrency" onChange={(e) => setSearchTerm(e.target.value)}></Input>
-            </div>
+            </div>)}
             <Row gutter={[32,32]} className="crypto-card-container">
                 {cryptos?.map((currency) => (
                     <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
