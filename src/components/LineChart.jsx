925a65fbda2c193f193017722057a42f50612260
Mateusz Kopij <sagasu@gmail.com>
2021-09-28 14:30:25 +0100

    Plot the chart

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -5,14 +5,48 @@ import {Col, Row, Typography} from 'antd';
 const {Title} = Typography;
 
 const LineChart = ({coinHistory, currentPrice, coinName}) => {
+    const coinPrice = [];
+    const coinTimestamp = [];
+    for(let i = 0;i<coinHistory?.data?.history?.length; i+=1){
+        coinPrice.push(coinHistory.data.history[i].price);
+        coinTimestamp.push(new Date(coinHistory.data.history[i].timestamp).toLocaleDateString());
+    }
+    const data = {
+        labels: coinTimestamp,
+        datasets: [
+            {
+                label: `Price in USD`,
+                data: coinPrice,
+                fill: false,
+                backgroundColor: '#0071bd',
+                borderColor: '#0071bd',
+            }
+        ]
+    }
+
+    const options = {
+        scales: {
+            yAxes: [
+                {
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }
+            ]
+        }
+    }
+
+    // Line is the one that plot the chart.
     return (
         <>
-          <Row className="chart-header">
+            <Row className="chart-header">
               <Title level={2} className="chart-title">{coinName} Price Chart</Title>
               <Col className="price-container"></Col>
               <Title level={5} className="price-change"> {coinHistory?.data?.change}%</Title>
               <Title level={5} className="current-price">Current {coinName} Price: $ {currentPrice}</Title>
-              </Row>  
+            </Row>
+
+            <Line data={data} options={options}/>  
         </>
     )
 }
