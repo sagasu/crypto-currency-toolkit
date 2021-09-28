635cc8f9a167e6e3a6b8a30815ea6354ebe8af56
Mateusz Kopij <sagasu@gmail.com>
2021-09-28 14:22:34 +0100

    introducing basic chart details

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -4,8 +4,9 @@ import { useParams } from 'react-router-dom';
 import millify from 'millify';
 import {Col, Row, Typography, Select} from 'antd';
 import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined,CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
+import LineChart  from './LineChart';
 
-import {useGetCryptoDetailsQuery} from '../services/cryptoApi';
+import {useGetCryptoDetailsQuery, useGetCryptoHistoryQuery} from '../services/cryptoApi';
 
 const {Title, Text} = Typography;
 const {Option} = Select;
@@ -13,13 +14,14 @@ const {Option} = Select;
 const CryptoDetails = () => {
     // useParams is great to get parameter w/o passing down props
     const {coinId} = useParams();
-    const[timePerdiod, setTimePeriod] = useState('7d');
+    const [timePeriod, setTimePeriod] = useState('7d');
     const {data, isFetching} = useGetCryptoDetailsQuery(coinId);
+    const {data: coinHistory} = useGetCryptoHistoryQuery({coinId, timePeriod});
     const cryptoDetails = data?.data?.coin;
     console.log(data);
 
     const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
-    if(!cryptoDetails) return 'Loading...';
+    if(!cryptoDetails || isFetching) return 'Loading...';
 
     const stats = [
         { title: 'Price to USD', value: `$ ${cryptoDetails.price && millify(cryptoDetails.price)}`, icon: <DollarCircleOutlined /> },
@@ -55,6 +57,8 @@ const CryptoDetails = () => {
               onChange={(value) => setTimePeriod(value)}>
                   {time.map((date) => <Option key={date}>{date}</Option>)}
               </Select>
+              <LineChart coinHistory={coinHistory} currentPrice={millify(cryptoDetails.price)} coinName={cryptoDetails.name}/>
+
               <Col className="stats-container">
                   <Col className="coin-value-statistics">
                       <Col className="coin-value-statistics-heading">
